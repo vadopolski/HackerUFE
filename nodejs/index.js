e887a92ed740d646d166821740b45f96509925de
Vadim Opolski <vaopolskij@gmail.com>
2019-01-22 22:22:15 +0300

    Added node project.

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,15 +1,29 @@
 console.log('YO');
 var httpServer = require('http');
+var url = require('url');
 var counter = 0;
 
+const server = httpServer.createServer(handleServerRequest);
 
-function handleServerRequest(request, response){
+function handleServerRequest(request, response) {
+    const parseURL = url.parse(request.url, true);
+    const originalPath = parseURL.pathname;
+
+    const querryStringObj = parseURL.query;
+    const {headers, method} = request;
+
+    console.log()
+
+
+
+
+    console.log('nUrl in Object', parseURL);
     console.log('>>>> NODE: got request', ++counter);
-    response.writeHead(200, {'Content-Type' : 'text/html'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('Hello World!!!! <br/>This is the response');
     response.end();
 }
 
-httpServer.createServer(handleServerRequest).listen(8888);
+server.listen(8888);
 
 console.log('>>>> Node Server');
